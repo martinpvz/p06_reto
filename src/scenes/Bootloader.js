a7aa119ad843c31ec3a57a47733b637a7878e78b
amg2592 <almorga2592@gmail.com>
2022-10-04 22:14:14 -0500

    Se corrigió animación Alhelí

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -1,5 +1,5 @@
-class Bootloader extends Phaser.Scene{
-    constructor(){
+class Bootloader extends Phaser.Scene {
+    constructor() {
         super({
             key: 'Bootloader'
         });
@@ -8,29 +8,27 @@ class Bootloader extends Phaser.Scene{
     init() {
         console.log('Escena Bootloader');
     }
-    
+
     preload() {
         this.load.path = './assets/';
-        this.load.atlas('alhe', 'alhe_idle/alhe.png','alhe_idle/alhe_atlas.json');
-        this.load.animation('alheAnim', 'alhe_idle/alhe_anim.json');
-        this.load.atlas('alheMov1', 'alhe_mov1/alhe_mov1.png','alhe_mov1/alhe_mov1_atlas.json');
-        this.load.animation('alheAnimMov1', 'alhe_mov1/alhe_anim1.json');
-        this.load.atlas('alheMov2', 'alhe_mov2/alhe_mov2.png','alhe_mov2/alhe_mov2_atlas.json');
-        this.load.animation('alheAnimMov2', 'alhe_mov2/alhe_anim2.json');
-
+        this.load.atlas('alhe', 'alheli/alheli.png', 'alheli/alheli.json');
     }
 
     create() {
-        this.alhe = this.add.sprite(200, 200, 'alhe', 0).setScale(.1);
-        this.alhe.anims.play('alhe_idle');
-        this.alheMov1 = this.add.sprite(400, 200, 'alheMov1', 0).setScale(.1);
-        this.alheMov1.anims.play('mov1_Alhe');
-        this.alheMov2 = this.add.sprite(600, 200, 'alheMov2', 0).setScale(.1);
-        this.alheMov2.anims.play('mov2_Alhe');
+        this.alheli = this.add.sprite(200, 200, 'alhe', 0);
+        //ANIMACIÓN ALHELÍ
+        this.anims.create({ key: 'idle', frames: this.anims.generateFrameNames('alhe', { prefix: 'alhe', suffix: '.png', start: 1, end: 7 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'baile', frames: this.anims.generateFrameNames('alhe', { prefix: 'baile0', suffix: '.png', start: 1, end: 8 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'golpe', frames: this.anims.generateFrameNames('alhe', { prefix: 'golpe0', suffix: '.png', start: 0, end: 6 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'rodillas', frames: this.anims.generateFrameNames('alhe', { prefix: 'rodillas0', suffix: '.png', start: 1, end: 9 }), repeat: -1, frameRate: 8 });
+        this.alheli.anims.play('golpe');
+
+
+
     }
 
     update(time, delta) {
-        
+
     }
 }
 
