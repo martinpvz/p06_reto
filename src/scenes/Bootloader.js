9dc7d4724c793d74701534c0c239f542b743d49a
Citla06 <cit.citla06@gmail.com>
2022-10-04 23:19:50 -0500

    Se agrega animacion para el personaje manuel

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -12,6 +12,9 @@ class Bootloader extends Phaser.Scene {
     preload() {
         this.load.path = './assets/';
         this.load.atlas('alhe', 'alheli/alheli.png', 'alheli/alheli.json');
+
+        //MANUEL
+        this.load.atlas('manu', 'manuel/manuel.png', 'manuel/manuel.json');
     }
 
     create() {
@@ -23,8 +26,13 @@ class Bootloader extends Phaser.Scene {
         this.anims.create({ key: 'rodillas', frames: this.anims.generateFrameNames('alhe', { prefix: 'rodillas0', suffix: '.png', start: 1, end: 9 }), repeat: -1, frameRate: 8 });
         this.alheli.anims.play('golpe');
 
-
-
+        this.manuel = this.add.sprite(600, 200, 'manu', 0);
+        //ANIMACION MANUEL
+        this.anims.create({ key: 'amen', frames: this.anims.generateFrameNames('manu', { prefix: 'amen', suffix: '.png', start: 1, end: 7 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'fuerte', frames: this.anims.generateFrameNames('manu', { prefix: 'fuerte', suffix: '.png', start: 1, end: 7 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'manu', frames: this.anims.generateFrameNames('manu', { prefix: 'manu', suffix: '.png', start: 1, end: 10 }), repeat: -1, frameRate: 8 });
+        this.anims.create({ key: 'patada', frames: this.anims.generateFrameNames('manu', { prefix: 'patada', suffix: '.png', start: 1, end: 9 }), repeat: -1, frameRate: 8 });
+        this.manuel.anims.play('fuerte');
     }
 
     update(time, delta) {
