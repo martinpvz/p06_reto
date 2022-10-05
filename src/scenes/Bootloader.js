4ae136d0086d0ca4b27fcf57a22e4dc1f9a99940
ManuelARs <m.a.r.s.232001@gmail.com>
2022-10-05 00:04:58 -0500

    prueba

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -13,19 +13,22 @@ class Bootloader extends Phaser.Scene {
         this.load.path = './assets/';
         this.load.image('Fondo2', 'Fondo2.png');
         this.load.image('wooden', 'woodframe.png');
+        this.load.image('RostroA', 'alheli/alhe_rostro.png');
         this.load.atlas('alhe', 'alheli/alheli.png', 'alheli/alheli.json');
     }
 
     create() {
         this.alheli = this.add.sprite(180, 260, 'alhe', 0);
         this.alheli.setScale(0.8);
+        this.alheliR = this.add.image(180, 260, 'RostroA');
+        this.alheliR.setScale(0.25);
 
         this.fondo = this.add.image(750, 360, 'Fondo2');
 
-        this.cuadro = this.add.image(180, 260, 'wooden').setInteractive().setName("alhe");
-        this.cuadro2 = this.add.image(550, 260, 'wooden').setInteractive();
-        this.cuadro3 = this.add.image(950, 260, 'wooden').setInteractive();
-        this.cuadro4 = this.add.image(1320, 260, 'wooden').setInteractive();
+        this.cuadro = this.add.image(180, 260, 'wooden').setInteractive().setName("alheli");
+        this.cuadro2 = this.add.image(550, 260, 'wooden').setInteractive().setName("alheli");
+        this.cuadro3 = this.add.image(950, 260, 'wooden').setInteractive().setName("alheli");
+        this.cuadro4 = this.add.image(1320, 260, 'wooden').setInteractive().setName("alheli");
 
         this.fondo.setScale(2.2);
         this.fondo.setDepth(-1);
@@ -43,19 +46,29 @@ class Bootloader extends Phaser.Scene {
         this.anims.create({ key: 'golpe', frames: this.anims.generateFrameNames('alhe', { prefix: 'golpe0', suffix: '.png', start: 0, end: 6 }), repeat: -1, frameRate: 8 });
         this.anims.create({ key: 'rodillas', frames: this.anims.generateFrameNames('alhe', { prefix: 'rodillas0', suffix: '.png', start: 1, end: 9 }), repeat: -1, frameRate: 8 });
         
-        var x="";
+        let rstro="alheliR";
+        
+        function rostro(rost) {
+            var j = rost+"R"
+            this.j.setAlpha(0);
+            // code here CAN use carName
+          }
+
         this.input.on(eventos.GAMEOBJECT_OVER, (pointer, gameObject) => {
             // this.pasar.play();
             gameObject.setTint(0xabdcd2);
             gameObject.setScale(0.35);
             console.log(gameObject.name);
-            x = gameObject.name;
-            this.x.anims.play('idle');
+            // x = gameObject.name+"R";
+            // console.log("ESTE ES X "+x);
+            // this.x = anims.play('idle');
             });
+
         this.input.on(eventos.GAMEOBJECT_OUT, (pointer, gameObject) => {
             gameObject.clearTint();
             gameObject.setScale(0.3);
-            this.alheli.anims.stopOnFrame(1);
+            this.alheliR.setAlpha(1);
+            // this.alheli.anims.stopOnFrame(1);
             });
 
 
